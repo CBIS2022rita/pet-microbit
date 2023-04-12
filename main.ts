ea1ee114939256affd8fe0272f56eee54510c9bb
CBIS2022rita <123551034+CBIS2022rita@users.noreply.github.com>
2023-04-12 10:57:13 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,20 @@ input.onButtonPressed(Button.A, function () {
     health += 1
     ANGRY.showImage(0)
 })
+input.onSound(DetectedSound.Loud, function () {
+    lightson = !(lightson)
+    if (lightson) {
+        basic.showLeds(`
+            . # # # .
+            . . # . .
+            . # . # .
+            # . . . #
+            # # # # #
+            `)
+    } else {
+        basic.clearScreen()
+    }
+})
 input.onGesture(Gesture.Shake, function () {
     basic.clearScreen()
     music.playTone(349, music.beat(BeatFraction.Whole))
@@ -9,8 +23,13 @@ input.onGesture(Gesture.Shake, function () {
     music.playTone(523, music.beat(BeatFraction.Whole))
     basic.showString("STOP SHAKING!")
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    HAPPY.showImage(0)
+})
 let turn = 0
+let lightson = false
 let health = 0
+let HAPPY: Image = null
 let ANGRY: Image = null
 ANGRY = images.createImage(`
     . # # # .
@@ -26,13 +45,16 @@ let NEUTRAL = images.createImage(`
     # # # # #
     . . . . .
     `)
-let HAPPY = images.createImage(`
+HAPPY = images.createImage(`
     . # # # .
     . . # . .
     # . # . #
     . # # # .
     . . . . .
     `)
+NEUTRAL.showImage(0)
+basic.showString("PRESS LOGO")
+input.setSoundThreshold(SoundThreshold.Loud, 128)
 loops.everyInterval(180000, function () {
     health += -1
     music.playTone(349, music.beat(BeatFraction.Whole))
@@ -53,9 +75,11 @@ basic.forever(function () {
     turn = input.rotation(Rotation.Pitch)
     if (turn >= 50 && turn <= 100) {
         HAPPY.showImage(0)
-    } else if (0 >= 0 && 0 <= 0) {
+    } else if (turn >= 0 && (0 as any) <= (49 as any)) {
         NEUTRAL.showImage(0)
+        basic.pause(5000)
     } else {
-    	
+        ANGRY.showImage(0)
+        basic.pause(5000)
     }
 })
